72edc99fc3d58d21ccdf1457836b075815d9d102
César Rodrigues <rasec1287@hotmail.com>
2017-06-29 09:43:15 -0300

    Adicionado spin loader na tela de cadastro

--- view/cadastro.php
+++ view/cadastro.php
@@ -1,37 +1,102 @@
-<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
-<script>
-	// Initialize Firebase
-	var config = {
-	apiKey: "AIzaSyDpWFgOQcdzjI1JVhATYAyGzIcI7XQCvTg",
-	authDomain: "eagora-be6d1.firebaseapp.com",
-	databaseURL: "https://eagora-be6d1.firebaseio.com",
-	projectId: "eagora-be6d1",
-	storageBucket: "eagora-be6d1.appspot.com",
-	messagingSenderId: "653607309132"
-	};
-	firebase.initializeApp(config);
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+	/* Center the loader */
+	#loader {
+	  position: absolute;
+	  left: 50%;
+	  top: 50%;
+	  z-index: 1;
+	  width: 150px;
+	  height: 150px;
+	  margin: -75px 0 0 -75px;
+	  border: 16px solid #f3f3f3;
+	  border-radius: 50%;
+	  border-top: 16px solid #3498db;
+	  width: 120px;
+	  height: 120px;
+	  -webkit-animation: spin 2s linear infinite;
+	  animation: spin 2s linear infinite;
+	}
 
-	// Get a reference to the database service
-	var database = firebase.database();
+	@-webkit-keyframes spin {
+	  0% { -webkit-transform: rotate(0deg); }
+	  100% { -webkit-transform: rotate(360deg); }
+	}
 
-	database.ref('animais/' + "acessorios/" + "<?php 
-			
-		$count = null;
-		$returnValue = preg_replace('/[.-]+/', '', $_POST['idCPF'], -1, $count);
+	@keyframes spin {
+	  0% { transform: rotate(0deg); }
+	  100% { transform: rotate(360deg); }
+	}
 
-		echo $returnValue	
-			
-		?>").set({
-          birth: "<?php echo $_POST['idDataNasc']; ?>",
-          email: "<?php echo $_POST['idEmail']; ?>",
-          name: "<?php echo $_POST['idNome']; ?>",
-          cpf: "<?php echo $_POST['idCPF']; ?>",
-          phone: "<?php echo $_POST['idTelefone']; ?>",
-          postalCode: "<?php echo $_POST['idCEP']; ?>",
-          rate: 0.01
-        }).then(() => {
-    		window.location = "../index.html";
-  		});
+	/* Add animation to "page content" */
+	.animate-bottom {
+	  position: relative;
+	  -webkit-animation-name: animatebottom;
+	  -webkit-animation-duration: 1s;
+	  animation-name: animatebottom;
+	  animation-duration: 1s
+	}
 
-        //window.location = "../index.html";
-</script>
+	@-webkit-keyframes animatebottom {
+	  from { bottom:-100px; opacity:0 } 
+	  to { bottom:0px; opacity:1 }
+	}
+
+	@keyframes animatebottom { 
+	  from{ bottom:-100px; opacity:0 } 
+	  to{ bottom:0; opacity:1 }
+	}
+
+	#myDiv {
+	  display: none;
+	  text-align: center;
+	}
+	</style>
+</head>
+	<div id="loader"></div>
+
+	<div style="display:none;" id="myDiv" class="animate-bottom">
+	</div>
+
+	<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+	<script>
+		// Initialize Firebase
+		var config = {
+		apiKey: "AIzaSyDpWFgOQcdzjI1JVhATYAyGzIcI7XQCvTg",
+		authDomain: "eagora-be6d1.firebaseapp.com",
+		databaseURL: "https://eagora-be6d1.firebaseio.com",
+		projectId: "eagora-be6d1",
+		storageBucket: "eagora-be6d1.appspot.com",
+		messagingSenderId: "653607309132"
+		};
+		firebase.initializeApp(config);
+
+		// Get a reference to the database service
+		var database = firebase.database();
+
+		database.ref('animais/' + "acessorios/" + "<?php 
+				
+			$count = null;
+			$returnValue = preg_replace('/[.-]+/', '', $_POST['idCPF'], -1, $count);
+
+			echo $returnValue	
+				
+			?>").set({
+	          birth: "<?php echo $_POST['idDataNasc']; ?>",
+	          email: "<?php echo $_POST['idEmail']; ?>",
+	          name: "<?php echo $_POST['idNome']; ?>",
+	          cpf: "<?php echo $_POST['idCPF']; ?>",
+	          phone: "<?php echo $_POST['idTelefone']; ?>",
+	          postalCode: "<?php echo $_POST['idCEP']; ?>",
+	          rate: 0.01
+	        }).then(() => {
+	        	document.getElementById("loader").style.display = "none";
+		  		document.getElementById("myDiv").style.display = "block";
+	    		window.location = "../index.html";
+	  		});
+	</script>
+
+</body>
+</html>
